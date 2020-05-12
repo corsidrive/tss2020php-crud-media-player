fa9e0fb428451aaf287cc334a6d6a76862adfb27
sarassoroberto <sarassoroberto@gmail.com>
2020-05-12 12:38:06 +0200

    classe DB non testata

---
+++ Config.php
@@ -0,0 +1,7 @@
+<?php
+class Config {
+    const DB_NAME = 'media_library';
+    const DB_HOST = 'localhost'; // 127.0.0.1
+    const DB_USER = 'root';
+    const DB_PASSWORD = 'fiacca';
+} 
---
+++ class/Db.php
@@ -0,0 +1,35 @@
+<?php
+//  $pdo = new PDO()
+//  $pdo->prepare()
+
+
+
+/**
+ * Db::getInstance();
+ */
+class Db {
+
+    private static $instance = null;
+
+    private function __construct() {}
+    
+    public static function getInstance()
+    {
+        // sono gia connesso ?
+        if(self::$instance === null){
+            
+            // no new PDO creo una nuova istanza
+            self::$instance = new PDO("mysql:host=".Config::DB_HOST.";dbname=".Config::DB_NAME,Config::DB_USER,Config::DB_PASSWORD); 
+            return self::$instance;
+            
+        }else{
+            
+            // si return PDO restituisco quella che ho creato prima
+            return self::$instance;
+        }
+
+
+    }
+
+
+}
