7730eef1d0b47c6fddb4ff8b7700209ccd3b5846
sarassoroberto <sarassoroberto@gmail.com>
2020-05-14 13:45:13 +0200

    FINE LEZIONE 14-5-2020 | ripulito il codice

--- autoload.php
+++ autoload.php
@@ -1,14 +1,6 @@
 <?php
-// error_reporting(0);
 spl_autoload_register(function($className){
-    // dove trovare le classi
-    $classPath = __DIR__.'/class/'.$className.'.php';
-    @include_once $classPath;
-   
-    $classPath = __DIR__.'/'.$className.'.php';
-    @include_once $classPath;
-    
-    $classPath = __DIR__.'/model/'.$className.'.php';
-    @include_once $classPath;
-    
+    @include_once __DIR__.'/class/'.$className.'.php';
+    @include_once __DIR__.'/'.$className.'.php';;
+    @include_once __DIR__.'/model/'.$className.'.php';
 });
--- test/DbTest.php
+++ test/DbTest.php
@@ -6,9 +6,3 @@ var_dump(Db::getInstance() === Db::getInstance());
 
 echo "deve essere un istanza del PDO \n";
 var_dump(is_a(Db::getInstance(),'PDO'));
-
-// 
-// "1" == 1  true
-// "1" === 1 false
-// 0 == false  | true
-// 0 === false | false
