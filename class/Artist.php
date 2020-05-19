03fe2fc2356bd75e3bb41991555599e41f3adee3
corsidrive <corsidrive@gmail.com>
2020-05-19 10:29:04 +0200

    Creato Artist Object VO

---
+++ class/Artist.php
@@ -0,0 +1,15 @@
+<?php
+class Artist {
+
+    public $artist_id;
+    public $name;
+
+}
+
+/*
+create table if not exists Artist
+(
+    artist_id int(11) not null auto_increment primary key,
+    name varchar(255) not null
+)
+*/
