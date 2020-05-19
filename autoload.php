3742b984f97df06bed4b97b07bc0c377fa5ae232
corsidrive <corsidrive@gmail.com>
2020-05-19 10:28:45 +0200

    tolto punto e virgola

--- autoload.php
+++ autoload.php
@@ -1,6 +1,6 @@
 <?php
 spl_autoload_register(function($className){
     @include_once __DIR__.'/class/'.$className.'.php';
-    @include_once __DIR__.'/'.$className.'.php';;
+    @include_once __DIR__.'/'.$className.'.php';
     @include_once __DIR__.'/model/'.$className.'.php';
 });
