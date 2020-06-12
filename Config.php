95cbfc6e3e9ad83faa38648e0b19a619780a5843
corsidrive <corsidrive@gmail.com>
2020-06-12 12:03:48 +0200

    cose

--- Config.php
+++ Config.php
@@ -1,12 +1,30 @@
 <?php
 
+// produzione (altervista)
 class Config {
     public const DB_NAME = 'media_library';
     public const DB_USER = 'root';
     public const DB_PASSWORD = 'fiacca';
     public const DB_HOST = 'localhost';
     
-    public const SITE_URL = 'http://localhost/tss2020php-crud-media-player/';
+    
+    // public const SITE_URL = 'http://localhost/tss2020php-crud-media-player/';
+    public const SITE_URL = 'http://xxxxxx/sito_media_library.php';
     public const VIEW_FOLDER = __DIR__.'/view/';
 
 }
+
+// sviluppo (laragon)
+
+// class Config {
+//     public const DB_NAME = 'media_library';
+//     public const DB_USER = 'root';
+//     public const DB_PASSWORD = 'fiacca';
+//     public const DB_HOST = 'localhost';
+    
+
+//     // public const SITE_URL = 'http://localhost/tss2020php-crud-media-player/';
+//     public const SITE_URL = 'http://xxxxxx/sito_media_library.php';
+//     public const VIEW_FOLDER = __DIR__.'/view/';
+
+// }
--- autoload.php
+++ autoload.php
@@ -1,6 +1,10 @@
 <?php
+// error_reporting(E_ALL);
+// Spegne gli errori
+// error_reporting(0);
 spl_autoload_register(function($className){
     @include_once __DIR__.'/class/'.$className.'.php';
     @include_once __DIR__.'/'.$className.'.php';
     @include_once __DIR__.'/model/'.$className.'.php';
 });
+
