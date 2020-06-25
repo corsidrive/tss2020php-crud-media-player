1ba369a1ed1007b9f12c7e2eee270855813d68a8
corsidrive <corsidrive@gmail.com>
2020-06-25 21:40:46 +0200

    Aggiunto avviso elenco canzoni

--- view/song_index_view.php
+++ view/song_index_view.php
@@ -1,13 +1,14 @@
-<?php View::render('head');?>
-<?php View::render('nav')?>
-<?php View::render('jumbotron', ['lead' => "elenco delle canzoni", 'site_name' => "Sito"]);?>
+<?php View::render('head'); ?>
+<?php View::render('nav') ?>
+<?php View::render('jumbotron', ['lead' => "elenco delle canzoni", 'site_name' => "Sito"]); ?>
 
-<?php //print_r($artisti)?>
+<?php //print_r($artisti)
+?>
 <div class="container mainBody">
 
-    <a href="<?=Config::SITE_URL . 'controller/song_add_controller.php'?>" >aggiungi canzone</a>
-
-<table class="table table-light">
+    <a href="<?= Config::SITE_URL . 'controller/song_add_controller.php' ?>">aggiungi canzone</a>
+    <?php if (count($songlist) > 0) { ?>
+        <table class="table table-light">
             <thead class="thead-light">
                 <tr>
                     <th width="1%">#</th>
@@ -19,31 +20,31 @@
                 </tr>
             </thead>
             <tbody>
-            <?php foreach ($songlist as $song) {?>
-                <tr>
-                    <td><?php echo $song->song_id; ?></td>
-                    <td> 
-                    <a href="<?php echo Config::SITE_URL . 'controller/song_edit_controller.php?id=' . $song->song_id?>">
-                    <?php echo $song->title; ?>
-                    </a>
-                    </td>
-                    <td><?php echo $song->nameGenre; ?></td>
-                    <td><?php echo $song->nameArtist; ?></td>
-                
-                <td>
-                        <audio controls>
-                            <source src="<?php echo Config::SITE_URL.'/uploads/'. $song->filename; ?>" type="audio/mpeg" />
-                        </audio>
-                </td>
-                <td class="text-center"><a href="<?=Config::SITE_URL . 'controller/song_delete_controller.php?id=' . $song->song_id?>" class="text-danger">delete</a></td>
+                <?php foreach ($songlist as $song) { ?>
+                    <tr>
+                        <td><?php echo $song->song_id; ?></td>
+                        <td>
+                            <a href="<?php echo Config::SITE_URL . 'controller/song_edit_controller.php?id=' . $song->song_id ?>">
+                                <?php echo $song->title; ?>
+                            </a>
+                        </td>
+                        <td><?php echo $song->nameGenre; ?></td>
+                        <td><?php echo $song->nameArtist; ?></td>
 
-                </tr>
+                        <td>
+                            <audio controls>
+                                <source src="<?php echo Config::SITE_URL . '/uploads/' . $song->filename; ?>" type="audio/mpeg" />
+                            </audio>
+                        </td>
+                        <td class="text-center">
+                            <a href="<?= Config::SITE_URL . 'controller/song_delete_controller.php?id=' . $song->song_id ?>" class="text-danger">delete</a>
+                        </td>
+                    </tr>
 
-
-            <?php }?>
+                <?php } // end foreach ?>
             </tbody>
-</table>
+        </table>
+    <?php } else { ?> <div class="alert alert-secondary"> nessuna canzone disponibile </div> <?php } ?>
+</div>
 
-            </div>
-
-<?php View::render('footer');?>
+<?php View::render('footer'); ?>
