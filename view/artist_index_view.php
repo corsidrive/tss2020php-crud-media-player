c73ef71d744bf0597d70de07704f78c683156112
corsidrive <corsidrive@gmail.com>
2020-06-25 21:58:04 +0200

    Artisti vuoto ok

--- view/artist_index_view.php
+++ view/artist_index_view.php
@@ -8,36 +8,36 @@
 
 <div class="container">
 
-    <a href="<?= Config::SITE_URL.'controller/artist_add_controller.php' ?>" >aggiungi artista</a>
+    <a href="<?= Config::SITE_URL . 'controller/artist_add_controller.php' ?>">aggiungi artista</a>
 
-    <table class="table table-light">
-        <thead class="thead-light">
-            <tr>
-                <th width="1%">#</th>
-                <th width="100%">Artist</th>
-                <th width="1%" class="text-center" colspan="2">Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-
-            <?php foreach ($artisti as  $artista) { ?>
-
+    <?php if ($artisti != null) { ?>
+        <table class="table table-light mt-3">
+            <thead class="thead-light">
                 <tr>
-                    <td><?php echo $artista->artist_id ?> </td>
-                   
-                    <td><a href="<?= Config::SITE_URL.'/controller/artist_edit_controller.php?id='.$artista->artist_id ?>"><?php echo $artista->name;  ?></a></td>
-                   
-                   
-                   
-                   
-                   
-                    <td class="text-center"><a href="<?= Config::SITE_URL.'controller/artist_delete_controller.php?id='.$artista->artist_id ?>" class="text-danger">delete</a></td>
+                    <th width="1%">#</th>
+                    <th width="100%">Artist</th>
+                    <th width="1%" class="text-center" colspan="2">Actions</th>
                 </tr>
+            </thead>
+            <tbody>
 
-            <?php } ?>
+                <?php foreach ($artisti as  $artista) { ?>
 
-        </tbody>
-    </table>
+                    <tr>
+                        <td><?php echo $artista->artist_id ?> </td>
+
+                        <td><a href="<?= Config::SITE_URL . '/controller/artist_edit_controller.php?id=' . $artista->artist_id ?>"><?php echo $artista->name;  ?></a></td>
+
+
+                        <td class="text-center"><a href="<?= Config::SITE_URL . 'controller/artist_delete_controller.php?id=' . $artista->artist_id ?>" class="text-danger">delete</a></td>
+                    </tr>
+
+                <?php } ?>
+
+            </tbody>
+        </table>
+    <?php } else { ?> <div class="alert alert-secondary mt-3"> nessuna artista disponibile </div> <?php } ?>
+
 </div>
 
 
