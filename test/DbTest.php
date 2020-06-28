6d5020c682c136e2d0f1782f7f8c6b12dfe8309f
corsidrive <corsidrive@gmail.com>
2020-06-28 20:24:06 +0200

    ripulito da vecchi test

--- test/DbTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once '../autoload.php';
-
-echo "la connessione deve essere sempre la stessa\n";
-var_dump(Db::getInstance() === Db::getInstance());
-
-echo "deve essere un istanza del PDO \n";
-var_dump(is_a(Db::getInstance(),'PDO'));
---
+++ test/bin/run.test.php
@@ -0,0 +1,7 @@
+<?php
+require_once "./autoload.php";
+require_once "./test/bin/testTools.php";
+require_once "./test/bin/dbTestTools.php";
+
+
+
