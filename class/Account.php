c4f17b4f4bcd2e7cdbe6a48f0baa4e548bbf4b7f
corsidrive <corsidrive@gmail.com>
2020-07-03 13:59:17 +0200

    account class

---
+++ class/Account.php
@@ -0,0 +1,83 @@
+<?php
+class Account {
+
+    private $id;
+    private $username;
+    private $password;
+    private $email;
+
+
+    public function __construct() {
+        $this->model = new AccountModel(Db::getInstance());
+    }
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of username
+     */ 
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set the value of username
+     *
+     * @return  self
+     */ 
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+   
+    public function save()
+    {
+        if(!$this->id){
+            $this->model->create($this);
+        }else{
+            $this->model->update($this);
+        }
+    }
+}
---
+++ model/AccountModel.php
@@ -0,0 +1,80 @@
+<?php
+class AccountModel {
+
+    public function __construct(PDO $pdo) {
+        $this->pdo = $pdo;
+    }
+
+    public function create(Account $account)
+    {
+
+        $stm = $this->pdo->prepare("INSERT INTO accounts
+                                    (username, password, email)
+                                    VALUES ( :username, :password, :email);");
+                                    
+        $stm->bindValue(':username',$account->getUsername());
+        $stm->bindValue(':password',$account->getPasswordHash());
+        $stm->bindValue(':email',$account->getEmail());
+
+        $stm->execute();
+    }
+
+    public function readOne($id)
+    {
+        $stm = $this->pdo->prepare('SELECT * FROM accounts where id = :id;');
+        $stm->bindValue(':id',$id);
+        $stm->execute();
+        $res = $stm->fetchAll(PDO::FETCH_CLASS,'Account');
+        
+        return count($res) > 0 ?  $res[0] : null ;
+    }
+
+    public function readAll($page=1,$itemsForPage=10)
+    {
+        $stm = $this->pdo->prepare('SELECT * FROM accounts ;');
+        $stm->execute();
+        $res = $stm->fetchAll(PDO::FETCH_CLASS,'Account');
+        
+        return count($res) > 0 ?  $res[0] : null ;
+    }
+    
+    public function update(Account $account)
+    {
+        $stm = $this->pdo->prepare('UPDATE  accounts
+                                            SET username=:username,
+                                            password=:password,
+                                            email=:email
+                                            
+                                            WHERE id=:id;');
+        
+        $stm->bindValue(':username',$account->getUsername(),PDO::PARAM_STR);
+        $stm->bindValue(':password',$account->getPasswordHash(),PDO::PARAM_STR);
+        $stm->bindValue(':email',$account->getEmail(),PDO::PARAM_STR);
+        $stm->bindValue(':id',$account->getId(),PDO::PARAM_INT);
+
+        $stm->execute();
+        return $stm->rowCount();
+    }
+    
+    public function delete($id)
+    {
+        
+    }
+
+    public function usernameExists($username):?Account
+    {
+        $stm = $this->pdo->prepare('SELECT * FROM accounts where username=:username;');
+        $stm->bindValue(':username',$username,PDO::PARAM_STR);
+        $stm->execute();
+        
+        $res = $stm->fetchAll(PDO::FETCH_CLASS,'Account');
+        return count($res) === 1 ? $res[0] : null; 
+    }
+    
+   
+
+   
+
+    
+}
+
